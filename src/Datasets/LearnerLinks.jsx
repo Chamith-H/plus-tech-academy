a1940a914bb4212d9851d2952d0c52577dbc410c
Chamith-H <chamith.softwaredev@gmail.com>
2023-04-15 11:19:56 +0530

    added

--- src/Datasets/LearnerLinks.jsx
+++ src/Datasets/LearnerLinks.jsx
@@ -133,6 +133,11 @@ const LearnerLinks = [
                         link:'https://www.workingnomads.com/jobs'
                     },
 
+                    {
+                        name:'dribbble.com',
+                        link:'https://www.dribbble.com'
+                    }
+
         ]
     },
 
@@ -141,6 +146,156 @@ const LearnerLinks = [
         title_Guide:'THROUGH THESE LINKS',
 
         navigate: [
+                    {
+                        name:'freeCodeCamp.org',
+                        link:'http://freeCodeCamp.org'
+                    },
+
+                    {
+                        name:'hexccoder.tech',
+                        link:'https://hexcoder.tech'
+                    },
+
+                    {
+                        name:'30seondsofcode.org',
+                        link:'https://www.30secondsofcode.org'
+                    },
+
+                    {
+                        name:'learnwithhusnain.com',
+                        link:'https://hu.learnwithhusnain.com'
+                    },
+
+                    {
+                        name:'beta.reactjs.org',
+                        link:'https://beta.reactjs.org'
+                    },
+
+                    {
+                        name:'edx.org',
+                        link:'https://www.edx.org'
+                    },
+
+                    {
+                        name:'futurelearn.com',
+                        link:'https://www.futurelearn.com'
+                    },
+
+                    {
+                        name:'w3schools.com',
+                        link:'https://www.w3schools.com'
+                    },
+
+                    {
+                        name:'learndigital.withgoogle.com',
+                        link:'https://learndigital.withgoogle.com'
+                    },
+
+                    {
+                        name:'freecodecamp.org',
+                        link:'https://www.freecodecamp.org'
+                    },
+
+                    {
+                        name:'hackdesign.org',
+                        link:'https://hackdesign.org'
+                    },
+
+                    {
+                        name:'scrimba.com',
+                        link:'https://scrimba.com'
+                    },
+
+                    {
+                        name:'learnpython.org',
+                        link:'https://www.learnpython.org'
+                    },
+
+                    {
+                        name:'cryptozombies.io',
+                        link:'https://cryptozombies.io'
+                    },
+
+                    {
+                        name:'digitaldefynd.com',
+                        link:'https://digitaldefynd.com'
+                    },
+
+                    {
+                        name:'ocw.mit.edu',
+                        link:'https://ocw.mit.edu/search/'
+                    },
+
+                    {
+                        name:'coursera.org',
+                        link:'https://www.coursera.org'
+                    },
+
+                    {
+                        name:'learn.shayhowe.com',
+                        link:'https://learn.shayhowe.com'
+                    },
+
+                    {
+                        name:'boolean-algebra.com',
+                        link:'https://www.boolean-algebra.com'
+                    },
+
+                    {
+                        name:'sql-practice.com',
+                        link:'https://www.sql-practice.com'
+                    },
+
+                    {
+                        name:'javascript.info',
+                        link:'https://javascript.info'
+                    },
+
+                    {
+                        name:'upskillcourses.com',
+                        link:'https://upskillcourses.com'
+                    },
+
+                    {
+                        name:'uicoach.io',
+                        link:'https://www.uicoach.io'
+                    },
+
+                    {
+                        name:'uxtools.co',
+                        link:'https://uxtools.co'
+                    },
+
+                    {
+                        name:'goodux.appcues.com',
+                        link:'https://goodux.appcues.com'
+                    },
+
+                    {
+                        name:'casestudy.club',
+                        link:'https://www.casestudy.club'
+                    },
+
+                    {
+                        name:'designcode.io',
+                        link:'https://designcode.io'
+                    },
+
+                    {
+                        name:'developedbyed.com',
+                        link:'https://developedbyed.com'
+                    },
+
+                    {
+                        name:'fireship.io',
+                        link:'https://fireship.io'
+                    },
+
+                    {
+                        name:'courses.webdevsimplified.com',
+                        link:'https://courses.webdevsimplified.com'
+                    },
+
                     {
                         name:'Bento - Learn to code and be a self-taught expert developer',
                         link:'https://bento.io/'
